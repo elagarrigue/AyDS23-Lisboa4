36c8eb6562f74f8510b32d784b5e7947eada7e6c
NahuelDP8 <ndp912@gmail.com>
2023-04-24 19:18:33 -0300

    Se aplico CleanCode para la clase DataBase
    Se eliminaron los logs.
    Se eliminaron los comentarios.
    Se hizo un refactor de la funciones saveArtist, getInfo y onCreate.
    Se añadio una funcion auxiliar para buscar el valor con el cursor.
    
    Co-Authored-By: Álvaro Mosconi <77434507+alvaromosconi@users.noreply.github.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -2,16 +2,12 @@ package ayds.lisboa.songinfo.moredetails.fulllogic
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.util.Log
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
 
 private const val DATABASE_NAME = "dictionary.db"
 private const val DATABASE_VERSION = 1
-
 private const val ARTISTS_TABLE = "artists"
 private const val ID_COLUMN = "id"
 private const val ARTIST_COLUMN = "artist"
@@ -21,13 +17,12 @@ private const val SOURCE_COLUMN = "source"
 class DataBase(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION)
 {
     private val projection = arrayOf(
-        "$ID_COLUMN",
-        "$ARTIST_COLUMN",
-        "$INFO_COLUMN"
+        ID_COLUMN,
+        ARTIST_COLUMN,
+        INFO_COLUMN
     )
 
-    override fun onCreate(db: SQLiteDatabase)
-    {
+    override fun onCreate(db: SQLiteDatabase) {
         val query: String =
             "CREATE TABLE $ARTISTS_TABLE (" +
                     "$ID_COLUMN string PRIMARY KEY, " +
@@ -37,35 +32,38 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, nul
                     ")"
 
         db.execSQL(query)
-
-        Log.i("DB", "DB created")
     }
 
-    fun getInfo(dbHelper: DataBase, artist: String): String? {
-
-        val db = dbHelper.readableDatabase
-
-        // How you want the results sorted in the resulting Cursor
-        val sortOrder = "$ARTIST_COLUMN DESC"
-        val cursor = db.query(
-            "$ARTISTS_TABLE",  // The table to query
-            projection,  // The array of columns to return (pass null to get all)
-            "$ARTIST_COLUMN  = ?",  // The columns for the WHERE clause
-            arrayOf(artist),  // The values for the WHERE clause
-            null,  // don't group the rows
-            null,  // don't filter by row groups
-            "$ARTIST_COLUMN DESC" // The sort order
-        )
-        val items: MutableList<String> = ArrayList()
-        while (cursor.moveToNext()) {
-            val info = cursor.getString(
-                cursor.getColumnIndexOrThrow("$INFO_COLUMN")
-            )
-            items.add(info)
+    fun saveArtist(artist: String, info: String) {
+        val values = ContentValues().apply {
+            put("ARTIST_COLUMN", artist)
+            put("INFO_COLUMN", info)
+            put("SOURCE_COLUMN", 1)
         }
-        cursor.close()
-        return if (items.isEmpty()) null else items[0]
+        writableDatabase?.insert(ARTISTS_TABLE, null, values)
     }
 
+    fun getInfo(artist: String): String? {
+        "$ARTIST_COLUMN DESC"
+        val cursor = readableDatabase.query(
+            ARTISTS_TABLE,
+            projection,
+            "$ARTIST_COLUMN  = ?",
+            arrayOf(artist),
+            null,
+            null,
+            "$ARTIST_COLUMN DESC"
+        )
+        return cursor.getInfo()
+    }
+    private fun Cursor.getInfo(): String? {
+        return try {
+            getString(getColumnIndexOrThrow(INFO_COLUMN))
+        } catch (e: IllegalArgumentException) {
+            null
+        } finally {
+            close()
+        }
+    }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 }
